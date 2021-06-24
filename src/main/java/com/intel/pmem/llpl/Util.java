d570db2c0c87a0e089b055464713df5b11e36b59
André Martins <amcmmm13@hotmail.com>
2021-06-25 00:28:04 +0100

    Typo on ExceptionInInitializerError message

--- src/main/java/com/intel/pmem/llpl/Util.java
+++ src/main/java/com/intel/pmem/llpl/Util.java
@@ -68,7 +68,7 @@ public class Util {
             loaded = true;
         } 
         catch (IOException e) {
-            throw new ExceptionInInitializerError("Failed to native load llpl library");
+            throw new ExceptionInInitializerError("Failed to load native llpl library");
         }
         finally {
             if (nativeLib != null) nativeLib.deleteOnExit();
